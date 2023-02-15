77e89abf6da72bf670c9602abe25b082fcbe07aa
MmeliD <125279240+MmeliD@users.noreply.github.com>
2023-02-15 16:42:24 +0200

    Add files via upload

---
+++ maps.py
@@ -0,0 +1,13 @@
+capitals = {
+ "ZAF": "Pretoria",
+ "BKA": "Ouagadougou",
+ "SEN": "Dakar"
+}
+print(capitals)
+
+capitals["SEN"] = "AAA","ABA"
+
+print(capitals)
+print(capitals["SEN"][1])
+
+
---
+++ sets.py
@@ -0,0 +1,6 @@
+odd = set([1,3,5,7,9])
+even = set([1,2,4,6,8])
+print (odd)
+print(even)
+in_both_sets = odd.intersection(even)
+print('Element in both sets is '+ str(in_both_sets))
